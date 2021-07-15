be94fa4d5fceea6de79c28f02f8895e5007d7278
shubharthaksangharsha <shubharthaksangharsha>
2021-07-16 03:05:34 +0530

    Added the display function to display current game

--- game.cpp
+++ game.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Game{
   vector<vector<char>> board;
 public:
+  Game();
   char did_win();
   char get(int i , int j);
   void display();
@@ -12,9 +13,29 @@ public:
   void playX(int i , int j);
 };
 
+//default constructor to initialize the board
+Game::Game(){
+    for(int i = 0; i < 3 ; i ++){
+      vector<char> v1(3,'-');    
+      board.push_back(v1);
+    }
+  }
+
+//Display function :-
+void Game:: display(){
+  for(auto i : board){
+    for(auto j : i){
+      cout<<"\t\t"<< j << " ";
+    }
+    cout<<endl<<endl;
+  }
+}
+
+
 //Driver Code:-
 int main(){
   Game g1;
-  
+
+  g1.display();
     return 0;
 }
