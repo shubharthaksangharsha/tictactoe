d20764b618a6090ddf7277f2a9e02a57c8b53328
shubharthaksangharsha <shubharthaksangharsha>
2021-07-16 03:11:08 +0530

    Added playX() and playO() function. Also added switch case and while loop in Driver code to make the game playable

--- game.cpp
+++ game.cpp
@@ -44,14 +44,49 @@ char Game:: get(int i, int j){
     return '-';
 }
 
+//playO function
+void Game:: playO(int i, int j){
+  board[i][j] = 'O';
+}
+
+//playX function
+void Game:: playX(int i, int j){
+  board[i][j] = 'X';
+}
+
 //Driver Code:-
 int main(){
+  char play,choice;
+  int i,j;
   Game g1;
-  int i , j ;
-  
+  cout<<"\t\t\tWelcome to Tic Tac Toe!!!!!"<<endl;
+  cout<<"Choose you want to play as Player1(O) or Player2(X): " ;
+  cin>>play;
+  cout<<"Press 1 and Hit enter  to display current game\n";
+  cout<<"Press 2 and input two numbers to check state of that position\n";
+  cout<<"Press 3 and input two numbers to play \n";
 
-  g1.display();
-  cin>>i >> j;
-  cout<<g1.get(i , j);
+    
+  while(cin>>choice){
+
+    switch(choice){
+    case '1' : 
+      g1.display();
+      break;
+    case '2' :
+      cin>>i>>j;
+      cout<<"Position of [" << i<<" ][" << j <<"] = "<< g1.get(i,j)<<endl;
+      break;
+    case '3' :
+      cin>> i>> j;
+      if(play == 'O'){
+	g1.playO(i , j);
+      }
+      else{
+	g1.playX(i,j);
+      }
+      
+    }
+  }
     return 0;
 }
